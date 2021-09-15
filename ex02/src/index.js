7dc4362a3d93122c2ab48bc002a365e2035d4a99
Amir Muharemovic <muharemovic.amir@gmail.com>
2021-09-15 16:42:06 +0200

    Dodao zadatke za treci dan

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change code below this line
+//Ovo je komentar u jednom redu
+/*Ovo je komentar
+na više redova*/
+    // Only change code above this line
+    
+}
+
+console.log(main());
+
+module.exports=main;
